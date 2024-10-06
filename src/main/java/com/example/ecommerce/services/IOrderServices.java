617a313225e3469aa6f701426c0e609fa6a221cd
Rodrigo Pereyra <rodrigopereyra02@gmail.com>
2024-10-05 23:08:46 -0300

    envio de email cuando se crea una orden

--- src/main/java/com/example/ecommerce/services/IOrderServices.java
+++ src/main/java/com/example/ecommerce/services/IOrderServices.java
@@ -3,12 +3,13 @@ package com.example.ecommerce.services;
 import com.example.ecommerce.api.dto.OrderDto;
 import com.example.ecommerce.domain.Enums.OrderStatus;
 import com.example.ecommerce.domain.models.User;
+import jakarta.mail.MessagingException;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface IOrderServices {
-    OrderDto createOrder(OrderDto orderDto, String userEmail);
+    OrderDto createOrder(OrderDto orderDto, String userEmail) throws MessagingException;
 
     List<OrderDto> getAllOrders();
 
